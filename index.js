14dfb372d563f39fa604fa6a2425ac01fffcf45e
hzhyvinskyi <hzhyvinskyi@gmail.com>
2019-01-24 21:01:47 +0200

    Added templates and main layout

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
+
+// Handlebars middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Index route
+app.get('/', (req, res) => {
+    const title = 'WELCOME!';
+    res.render('index', {
+        title: title
+    });
+});
+
+// Aboute route
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 const port = 3000;
 
