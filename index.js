27598f875880a039f085d59e435c3852406db1bb
hzhyvinskyi <hzhyvinskyi@gmail.com>
2019-01-25 23:46:23 +0200

    Added body parser and validation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -11,11 +12,21 @@ mongoose.connect('mongodb://localhost/vjp-dev', {
   .then(() => console.log('MongoDB was successfully connected!'))
   .catch(err => console.log(err));
 
+// Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
 // Handlebars middleware
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
 
 // Index route
 app.get('/', (req, res) => {
@@ -33,6 +44,27 @@ app.get('/about', (req, res) => {
 // Add idea form
 app.get('/ideas/add', (req, res) => {
     res.render('ideas/add');
+});
+
+app.post('/ideas', (req, res) => {
+    let err = [];
+
+    if (!req.body.title) {
+        err.push({text: 'Please add a title'});
+    }
+    if (!req.body.details) {
+        err.push({text: 'Please add some details'});
+    }
+
+    if (err.length) {
+        res.render('ideas/add', {
+            err: err,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else {
+        res.send('passed');
+    }
 });
 
 const port = 3000;
